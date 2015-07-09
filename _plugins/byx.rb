ac871b865d143b716f8752521f39a4cef6680417
Dustin J. Mitchell <dustin@mozilla.com>
2015-07-08 20:23:30 -0400

    sort byx pages

--- _plugins/byx.rb
+++ _plugins/byx.rb
@@ -1,22 +1,23 @@
 module ByX
   # for each by_xyz page, collect recipes tagged with 'xyz: [a, b]'
-  # into matches, keyed by `a` and `b`.
+  # into matches, [['a', [recipe1, recipe2]], ['b': [recipe3]]], sorted
   class Generator < Jekyll::Generator
     def generate(site)
       site.pages.each do |page|
         next if ! page.name.start_with? 'by_'
         attr = page.name[3..-6]
-        matches = page.data['matches'] = {}
+        matchmap = {}
         site.collections['recipes'].docs.each do |p|
           if p.data.include? attr
             p.data[attr].each do |v|
-              if ! matches.include? v
-                matches[v] = []
+              if ! matchmap.include? v
+                matchmap[v] = []
               end
-              matches[v] << p
+              matchmap[v] << p
             end
           end
         end
+        matches = page.data['matches'] = matchmap.map {|x, y| [x, y]}.sort
       end
     end
   end
